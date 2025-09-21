dee997b2673723c1cee0133756ea9b3653301474
venkat-jaswanth <jaswanth1731@gmail.com>
2025-09-21 15:57:00 +0530

    21- 09 - 25

---
+++ Strivers SDE Sheet/arrays/1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int , int> mp;
+        for (int i = 0 ; i < nums.size() ; i++ ){
+            int num = nums[i];
+            int need = target - num;
+            if(mp.find(need)!= mp.end()) return {mp[need],i};
+            mp[num] = i;
+        }
+        return {-1,-1};
+    }
+};
--- Strivers SDE Sheet/arrays/62.cpp
+++ Strivers SDE Sheet/arrays/62.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+
+// TOP DOWN APPROACH WITH MEMOIZATION
 class Solution {
 public:
     int countPaths(int i, int j, int n, int m , vector<vector<int>> &dp){
@@ -18,5 +20,31 @@ public:
 
 };
 
+// BOTTOM UP APPROACH WITH TABULATION
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+    vector<vector<int>> dp(m, vector<int>(n, 1));
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        }
+    }
+    return dp[m-1][n-1];
+}
+};
 
 
+// OPTIMIZED SPACE COMPLEXITY
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> dp(n, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] = dp[j] + dp[j-1];
+            }
+        }
+        return dp[n-1];
+    }
+};
