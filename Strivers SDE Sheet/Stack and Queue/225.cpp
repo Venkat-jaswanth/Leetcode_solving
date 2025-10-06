2d0c048aac028461e01b43a7bb2600bd3d613979
venkat-jaswanth <jaswanth1731@gmail.com>
2025-10-06 22:31:48 +0530

    06-10-25

--- Strivers SDE Sheet/Stack and Queue/225.cpp
+++ Strivers SDE Sheet/Stack and Queue/225.cpp
@@ -45,6 +45,45 @@ public:
 
 
 
+// using 2 queues
+
+#include <queue>
+using namespace std;
+
+class MyStack {
+public:
+    queue<int> q1, q2;
+
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        
+        q2.push(x);
+        
+        while (!q1.empty()) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        
+        swap(q1, q2);
+    }
+    
+    int pop() {
+        int n = q1.front();
+        q1.pop();
+        return n;
+    }
+    
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
 
 
 
