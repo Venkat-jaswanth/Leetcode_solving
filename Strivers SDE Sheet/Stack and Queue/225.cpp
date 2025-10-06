d9167252ab2aaf467d39671f04cdca36378dcd71
venkat-jaswanth <jaswanth1731@gmail.com>
2025-10-06 22:29:56 +0530

    6-10-25

---
+++ Strivers SDE Sheet/Stack and Queue/225.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class MyStack {
+public:
+    queue<int> q;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        for(int i = 0; i<q.size()-1; i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int n = q.front();
+        q.pop();
+        return n;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return (q.size() > 0) ? false : true;
+    }
+};
+
+
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+
+
+
+
+
+
---
+++ Strivers SDE Sheet/heaps/215.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) 
+    {
+        nth_element(nums.begin(), nums.begin() + (nums.size() - k), nums.end());
+        return nums[nums.size() - k];                                        
+    }
+}; 
