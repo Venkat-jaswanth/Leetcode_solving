80430ce8fe70b741448c36b5dac612722aee6150
venkat-jaswanth <jaswanth1731@gmail.com>
2025-11-14 14:59:07 +0530

    14 -11-2025

--- ALGORITHMS/linear_sieve.cpp
+++ ALGORITHMS/linear_sieve.cpp
@@ -1,31 +1,82 @@
 #include <bits/stdc++.h>
-
+#include <time.h>
 using namespace std;
 
-using vv = vector<int>;
+using vv = vector<long long>;
 vv primes;
 vv ls;
-void linearsieve(int n){
-    ls.assign(n+1,0);
-    for(int i = 2; i<n; i++){
-        if(ls[i] == 0){
+
+
+void linearsieve(long long n)
+{
+    ls.assign(n + 1, 0);
+    for (long long i = 2; i < n; i++)
+    {
+        if (ls[i] == 0)
+        {
             primes.push_back(i);
             ls[i] = i;
         }
-        for(auto p:primes){
-            if((i*p)>n || p>ls[i]) break;
-            ls[i*p] = p;
+        for (auto p : primes)
+        {
+            if ((i * p) > n || p > ls[i])
+                break;
+            ls[i * p] = p;
         }
     }
 }
 
-int main(){
-    int n;
-    cin >> n;
-    cout << '\n';
-    linearsieve(n);
-    for(auto p : primes){
-        cout << p << ",";
+vector<long long> sieve(long long n)
+{
+    primes.clear();
+    primes.assign(n + 1, true);
+    // creation of boolean array
+    for (long long p = 2; p * p <= n; p++)
+    {
+        if (primes[p] == true)
+        {
+
+            // marking as false
+            for (long long i = p * p; i <= n; i += p)
+                primes[i] = false;
+        }
     }
 
+    vector<long long> res;
+    for (int p = 2; p <= n; p++)
+    {
+        if (primes[p])
+        {
+            res.push_back(p);
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    long long n;
+    cin >> n;
+    double start = clock();
+    cout << "Linear Sieve: \n";
+    cout << '\n';
+    linearsieve(n);
+    double end = clock();
+    cout << "Time: " << (end - start) / CLOCKS_PER_SEC << " sec\n";
+    cout << "Number of primes found: " << primes.size() << '\n';
+    // for(auto p : primes){
+    //     cout << p << ",";
+    // }
+    cout << '\n';
+    cout << "------------------\n";
+    cout << "Sieve of Eratosthenes: Normal \n";
+    start = clock();
+    vector<long long> res = sieve(n);
+    end = clock();
+    cout << "Time: " << (end - start) / CLOCKS_PER_SEC << " sec\n";
+    cout << "Number of primes found: " << res.size() << '\n';
+    // for(auto p : res){
+    //     cout << p << ",";
+    // }
+    cout << '\n';
 }
---
+++ OA PRACTICE/AMAZON/klees.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int solve(vector<pair<int,int>>& guys) {
+    sort(guys.begin(), guys.end(),
+         [](auto &a, auto &b) {
+             if(a.first == b.first)
+                 return a.second > b.second; 
+             return a.first < b.first;
+         });
+    int active = 0, max_people = 0;
+    for(auto &g : guys) {
+        active += g.second;
+        max_people = max(max_people, active);
+    }
+    return max_people;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int start , end;
+    vector<pair<int,int>> guys;
+    guys.reserve(2*n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> start >> end;
+        guys.push_back({start,+1});
+        guys.push_back({end,-1});
+    }
+
+    int result = solve(guys);
+    cout << result;
+    return 0;
+}
+// 5
+// 0900 1000
+// 1000 1100
+// 1200 1300
+// 1400 1500
+// 1500 1600
+
+
+
+// int solve(vector<pair<int,int>>& intervals) {
+//     priority_queue<int, vector<int>, greater<int>> pq;
+//     int max_overlap = 0;
+
+//     for(auto &inv : intervals) {
+//         int start = inv.first;
+//         int end   = inv.second;
+
+//         while(!pq.empty() && pq.top() <= start) {
+//             pq.pop();
+//         }
+//         pq.push(end);
+//         max_overlap = max(max_overlap, (int)pq.size());
+//     }
+//     return max_overlap;
+// }
