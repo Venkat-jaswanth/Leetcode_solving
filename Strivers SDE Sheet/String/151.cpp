495e7e0e1cd487d20ceea3a0d03d3d8816aa1364
venkat-jaswanth <jaswanth1731@gmail.com>
2025-12-06 23:01:36 +0530

    5-12-25

---
+++ Strivers SDE Sheet/String/151.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.length() <= 1) return s;
+        int n = s.length();
+        string maxStr = s.substr(0, 1);
+        auto expand = [&](int left, int right) {
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                left--;
+                right++;
+            }
+            return s.substr(left + 1, right - left - 1);
+        };
+        for (int i = 0; i < n - 1; i++) {
+            string odd = expand(i, i);
+            string even = expand(i, i + 1);
+            if (odd.length() > maxStr.length()) maxStr = odd;
+            if (even.length() > maxStr.length()) maxStr = even;
+        }
+        return maxStr;
+    }
+};
